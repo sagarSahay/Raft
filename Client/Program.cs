3ad5a7b8304fc282798743c3df4af0d1dacb1e19
sagarSahay <sagar.sahay@gmail.com>
2021-07-30 10:19:10 +0100

    Init

---
+++ Client/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Client
+{
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Client.StartClient();
+            return 0;
+        }
+    }
+
+    public class Client
+    {
+        public static void StartClient()
+        {
+            var bytes = new byte[1024];
+
+            try
+            {
+                var ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                var ipAddress = ipHostInfo.AddressList[0];
+                var remoteEP = new IPEndPoint(ipAddress, 11000);
+
+                var sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                    sender.Connect(remoteEP);
+                    Console.WriteLine($"Socket connected to {sender.RemoteEndPoint.ToString()}");
+
+                    while (true)
+                    {
+                        var msg = Console.ReadLine();
+                        var bytesSent = sender.Send(Encoding.ASCII.GetBytes(msg));
+                        var bytesRec = sender.Receive(bytes);
+                        Console.WriteLine($"Echoed test = {Encoding.ASCII.GetString(bytes, 0, bytesRec)}");
+                        if (msg.Contains("<EOF>", StringComparison.OrdinalIgnoreCase))
+                        {
+                            break;
+                        }
+                    }
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine($"Argument null exception {ane.ToString()}");
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine($"SocketException {se.ToString()}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Unexpected exception {e.ToString()}");
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Server
+{
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Server.StartListening();
+            return 0;
+        }
+    }
+
+    public class Server
+    {
+        public static string data = null;
+        public static Dictionary<string, string> state = new Dictionary<string, string>();
+
+        public static void StartListening()
+        {
+            var bytes = new byte[1024];
+
+            var ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            var ipAddress = ipHostInfo.AddressList[0];
+            var localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+            var listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection ....");
+                    var handler = listener.Accept();
+                    data = null;
+
+                    while (true)
+                    {
+                        var bytesRec = handler.Receive(bytes);
+                        data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                        Console.WriteLine($"Text received : {data}");
+
+                        var dataArr = data.Split(" ");
+                        var command = dataArr[0];
+                        var key = dataArr[1];
+                        var value = dataArr[2];
+                        
+                        var msg = Encoding.ASCII.GetBytes(data);
+                        handler.Send(msg);
+                        if (data.Contains("<EOF>"))
+                        {
+                            Console.WriteLine("break condition hit");
+                            break;
+                        }
+                    }
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                    break;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            
+            Console.WriteLine("\nPress ENTER to continue ....");
+            Console.Read();
+
+        }
+    }
+}
